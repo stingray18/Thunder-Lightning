b198eb6d165d8bfbced56f19141749e8e7ad3e66
Ingo <ingo@patty.ingo-home.local>
2026-02-07 17:35:00 +0100

    adapt to new syntax

--- TnL-source/src/signals.h
+++ TnL-source/src/signals.h
@@ -5,22 +5,22 @@
 #include <sigc++/sigc++.h>
 
 // ActionSignal
-typedef sigc::signal<void> ActionSignal;
-typedef sigc::slot<void> ActionSlot;
+typedef sigc::signal<void()> ActionSignal;
+typedef sigc::slot<void()> ActionSlot;
 
 // SwitchSignal
 // on/off = true/false
 typedef sigc::signal<void, bool> SwitchSignal;
-typedef sigc::slot<void, bool> SwitchSlot;
+typedef sigc::slot<void(bool)> SwitchSlot;
 
 // KeyboardSignal
 // Keycode, pressed
 typedef sigc::signal<void, int, bool> KeyboardSignal;
-typedef sigc::slot<void, int, bool> KeyboardSlot;
+typedef sigc::slot<void(int, bool)> KeyboardSlot;
 
 // MouseMoveSignal
 // x,y
 typedef sigc::signal<void, int, int> MouseMoveSignal;
-typedef sigc::slot<void, int, int> MouseMoveSlot;
+typedef sigc::slot<void(int, int)> MouseMoveSlot;
 
 #endif
